2037dd4b7b3e6c34c1a91bd25634d469ddce81f7
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2024-09-16 12:25:57 +0600

    push by Rasel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
- 
+import Rasel from "./Pages/Rasel";
+
+
 function App() {
 
   return (
     <>
- 
-      <h1 className="text-center font-bold text-rose-700 text-5xl mt-20">Test By Rasel</h1>
- 
- 
+
+      <Rasel></Rasel>
     </>
   );
 }
---
+++ src/Pages/Rasel.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const Rasel = () => {
+    return (
+        <div>
+            <h1 className="text-center font-bold text-rose-700 text-5xl mt-20">Test By Rasel</h1>
+
+        </div>
+    );
+};
+
+export default Rasel;
